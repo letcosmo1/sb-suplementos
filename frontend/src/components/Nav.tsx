bf3626f578dc05a418c543fea98951a3dd2040d9
Leticia Cosmo <letcosmogeller@gmail.com>
2024-04-12 23:13:29 -0300

    inibir nav para adm

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -6,25 +6,34 @@ type PropTypes = {
 }
 
 const Nav = ({ isLoggedIn }: PropTypes) => {
-    const location = useLocation();
+    const location = useLocation()
+
+    const renderLinks = () => {
+        if(location.pathname === "/admin/login") {
+            return <ul></ul>
+        }
+
+        if(!isLoggedIn) {
+            return  <ul>
+                        <li><Link to={"/produtos"}>Todos os produtos</Link></li>
+                        <li><Link to={"/produtos?categoria=Whey"}>Whey</Link></li>
+                        <li><Link to={"/produtos?categoria=Creatina"}>Creatina</Link></li>
+                        <li><Link to={"/produtos?categoria=Pré-treino"}>Pré-treino</Link></li>
+                        <li><Link to={"/produtos?categoria=Hipercalórico"}>Hipercalórico</Link></li>
+                    </ul>
+        }
+        
+        return  <ul style={{ justifyContent: "center" }}>
+                    <li><Link to={"/produtos"}>Todos os produtos</Link></li>
+                </ul>
+    }
 
     return (
         
         <nav className="barra-navegacao">
-            {location.pathname !== "/login" || !isLoggedIn && 
             <ul>
-                <li><Link to={"/produtos"}>Todos os produtos</Link></li>
-                <li><Link to={"/produtos?categoria=Whey"}>Whey</Link></li>
-                <li><Link to={"/produtos?categoria=Creatina"}>Creatina</Link></li>
-                <li><Link to={"/produtos?categoria=Pré-treino"}>Pré-treino</Link></li>
-                <li><Link to={"/produtos?categoria=Hipercalórico"}>Hipercalórico</Link></li>
+                { renderLinks() }
             </ul>
-            }
-            {isLoggedIn &&
-            <ul style={{ justifyContent: "center" }}>
-                <li><Link to={"/produtos"}>Todos os produtos</Link></li>
-            </ul>
-            }
         </nav>
     )
   }
