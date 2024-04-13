e9a126133915cf2e5fdce39f0a49b8af7b4a3206
Kledson Vinicius <kledsonvinicius0702@gmail.com>
2024-04-13 15:00:02 -0300

    fix bug front

--- frontend/src/api/ProductsApi.ts
+++ frontend/src/api/ProductsApi.ts
@@ -4,66 +4,72 @@ import {
   TypeSaleProduct,
 } from "../utils/Types";
 
-const api_url: string = "http://localhost:3000"
+const api_url: string = "http://localhost:3000";
 
 export const getHighlightCategories = () => {
   return fetch(`${api_url}/categories/hl`)
     .then((res) => res.json())
-    .then((data: TypeHighlightCategory[]) => data)
-}
+    .then((data: TypeHighlightCategory[]) => data);
+};
 export const getAllProductsAdm = (token: string) => {
   return fetch(`${api_url}/admin`, {
     method: "GET",
-    headers: { "authorization": `Bearer ${token}` }
+    headers: { authorization: `Bearer ${token}` },
   })
     .then((res) => res.json())
-    .then((data: TypeProduct[]) => data)
-}
+    .then((data: TypeProduct[]) => data);
+};
 export const getAllProducts = (order: string) => {
   return fetch(`${api_url}/products/${order}`)
     .then((res) => res.json())
-    .then((data: TypeProduct[]) => data)
-}
+    .then((data: TypeProduct[]) => data);
+};
 export const getProductsByName = (name: string) => {
   return fetch(`${api_url}/products/${name}`)
     .then((res) => res.json())
-    .then((data: TypeProduct[]) => data)
-}
+    .then((data: TypeProduct[]) => data);
+};
 export const getProductById = (id: string | undefined) => {
   return fetch(`${api_url}/product/${id}`)
     .then((res) => res.json())
-    .then((data: TypeProduct) => data)
-}
+    .then((data: TypeProduct) => data);
+};
 export const getProductsByCategory = (order: string, category: string) => {
   return fetch(`${api_url}/products/category/${order}/${category}`)
     .then((res) => res.json())
     .then((data: any) => {
-      let products: TypeProduct[] = []
+      let products: TypeProduct[] = [];
 
-      if (!data.message) products = data
+      if (!data.message) products = data;
 
-      return products
-    })
-}
+      return products;
+    });
+};
 export const sendProductForSale = (product: TypeSaleProduct) => {
   return fetch(`${api_url}/sale`, {
-          method: "POST",
-          headers: { "Content-type": "application/json" },
-          body: JSON.stringify({ product: product })
-        })
-        .then((res) => res.json())
-        .then((data) => {
-          let url = ""
-          if(data.url) url = data.url
-          return url
-        })
-}
-export const updateProductAvailableAdm = (token: string, _id: string, available: boolean) => {
-  return fetch(`${api_url}/admin/patch/${_id}`, {
-    method: "PATCH",
-    headers: { "authorization": `Bearer ${token}` },
-    body: JSON.stringify({ available: available })
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({ product: product }),
   })
     .then((res) => res.json())
-    .then(console.log)
-}
+    .then((data) => {
+      let url = "";
+      if (data.url) url = data.url;
+      return url;
+    });
+};
+export const updateProductAvailableAdm = (
+  token: string,
+  _id: string,
+  available: boolean
+) => {
+  return fetch(`${api_url}/admin/patch/${_id}`, {
+    method: "PATCH",
+    headers: {
+      authorization: `Bearer ${token}`,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ available: available }),
+  }).then((res) => res.json());
+};
--- frontend/src/pages/PaginaDetalhes.tsx
+++ frontend/src/pages/PaginaDetalhes.tsx
@@ -1,142 +1,156 @@
-import './PaginaDetalhes.css'
-import { Link, useParams } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-import { TypeProduct, TypeSaleProduct } from '../utils/Types';
-import { getImage } from '../utils/ImageUrl';
-import { useEffect, useState } from 'react';
-import { getProductById, sendProductForSale, updateProductAvailableAdm } from '../api/ProductsApi';
-import { capitalizeFirstLetter, toReais } from '../utils/StringFormat';
+import "./PaginaDetalhes.css";
+import { Link, useParams } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { TypeProduct, TypeSaleProduct } from "../utils/Types";
+import { getImage } from "../utils/ImageUrl";
+import { useEffect, useState } from "react";
+import {
+  getProductById,
+  sendProductForSale,
+  updateProductAvailableAdm,
+} from "../api/ProductsApi";
+import { capitalizeFirstLetter, toReais } from "../utils/StringFormat";
 
 type PropTypes = {
-    isLoggedIn: boolean
-}
+  isLoggedIn: boolean;
+};
 
-const PaginaDetalhes = ({ isLoggedIn }:PropTypes) => {
-    const { id } = useParams()
+const PaginaDetalhes = ({ isLoggedIn }: PropTypes) => {
+  const { id } = useParams();
 
-    const [product, setProduct] = useState<TypeProduct>({
-        _id: "", 
-        category: "",
-        image: "",
-        name: "",
-        price: 0,
-        description: "",
-        flavor: "",
-        weight: "",
-        table: "",
-        available: true
-    })
+  const [product, setProduct] = useState<TypeProduct>({
+    _id: "",
+    category: "",
+    image: "",
+    name: "",
+    price: 0,
+    description: "",
+    flavor: "",
+    weight: "",
+    table: "",
+    available: true,
+  });
 
-    const [selectedImage, setSelectedImage] = useState<string>("")
+  const [selectedImage, setSelectedImage] = useState<string>("");
 
-    const [available, setAvailable] = useState<boolean>(true)
+  const [available, setAvailable] = useState<boolean>(true);
 
-    const toggleImage = (imagem_url: string) => {
-        setSelectedImage(imagem_url)
+  const toggleImage = (imagem_url: string) => {
+    setSelectedImage(imagem_url);
+  };
+
+  const whatsappRedirect = () => {
+    const sale_product: TypeSaleProduct = {
+      name: product.name,
+      price: product.price,
+      flavor: product.flavor,
+    };
+
+    sendProductForSale(sale_product).then((data) =>
+      window.open(data, "_blank")
+    );
+  };
+
+  const handleAvailableChange = () => {
+    const token: string | null = localStorage.getItem("token");
+
+    if (isLoggedIn && token) {
+      updateProductAvailableAdm(token, product._id, !available);
+      let updated_product: TypeProduct = product;
+      updated_product.available = !available;
+      setProduct(updated_product);
+      setAvailable(!available);
     }
+  };
 
-    const whatsappRedirect = () => {
-        const sale_product: TypeSaleProduct = {
-            name: product.name,
-            price: product.price,
-            flavor: product.flavor
+  useEffect(() => {
+    getProductById(id).then((data) => {
+      setProduct(data);
+      setAvailable(data.available);
+      setSelectedImage(data.image);
+    });
+  }, []);
+
+  return (
+    <main className="pagina-detalhes">
+      <nav className="detalhes-breadcrumbs">
+        <Link to={"/"}>Página Inicial</Link>
+        {">"}
+        <Link to={"/produtos"}>Produtos</Link>
+        {">"}
+        {
+          <Link to={`/produtos?categoria=${product.category}`}>
+            {product.category}
+          </Link>
         }
+        {">"}
+        <Link to={`/produto/${id}`}>{capitalizeFirstLetter(product.name)}</Link>
+      </nav>
 
-        sendProductForSale(sale_product)
-            .then(data => window.open(data, '_blank'))
-    }
-
-    const handleAvailableChange = () => {
-        const token: string | null = localStorage.getItem("token")
-
-        if(isLoggedIn && token) {
-            updateProductAvailableAdm(token, product._id, !available)
-            let updated_product: TypeProduct = product
-            updated_product.available = !available
-            setProduct(updated_product)
-            setAvailable(!available)
-        }
-    }
-    
-    useEffect(() => {
-        getProductById(id)
-            .then(data => {
-                setProduct(data)
-                setAvailable(data.available)
-                setSelectedImage(data.image)
-            })
-    }, []);
-
-    return (
-        <main className="pagina-detalhes">
-            <nav className="detalhes-breadcrumbs">
-                <Link to={"/"}>Página Inicial</Link>
-                { ">" }
-                <Link to={"/produtos"}>Produtos</Link>
-                { ">" }
-                {<Link to={`/produtos?categoria=${product.category}`}>{ product.category }</Link> }
-                { ">" }
-                <Link to={`/produto/${id}`}>{ capitalizeFirstLetter(product.name)  }</Link>
-            </nav>
-
-            <div className="detalhes-container">
-                <section className="produto-imagens">  
-                    <div className="img-nav">
-                        <div onClick={ () => toggleImage(product.image) }>
-                            <img src={ getImage(product.image) } />
-                        </div>
-                        { product.table &&
-                        <div onClick={ () => toggleImage(product.table) }>
-                            <img src={ getImage(product.table) } />
-                        </div>
-                        }
-                    </div>
-                    
-                    <div className="img-selecionada">
-                        <img src={ getImage(selectedImage) } />
-                    </div>
-                </section>
-
-                <section className="produto-info">
-                    <h2>{ product.name }</h2>
-                    {product.flavor &&
-                    <div>
-                        <h3>Sabor</h3>
-                        <p>{ product.flavor }</p>
-                    </div>
-                    }
-                    {product.weight &&
-                    <div>
-                        <h3>Peso</h3>
-                        <p>{ product.weight }</p>
-                    </div>
-                    }
-                </section>
-                <div className="produto-compras">
-                    <section className="produto-disponivel">
-                        <h3>{ product.available ? "Em estoque" : "Indisponível" }</h3>   
-                        { isLoggedIn &&
-                        <input type="checkbox" checked={ available } onChange={ handleAvailableChange } />
-                        }   
-                    </section>
-                    <section className="produto-preco">
-                        <h3>Preço</h3>
-                        <p>{ toReais(product.price) }</p>
-                        <button onClick={ whatsappRedirect }><FontAwesomeIcon icon={faWhatsapp} />COMPRE AGORA</button>
-                    </section>
-                </div>
+      <div className="detalhes-container">
+        <section className="produto-imagens">
+          <div className="img-nav">
+            <div onClick={() => toggleImage(product.image)}>
+              <img src={getImage(product.image)} />
             </div>
-            
-            <section className="produto-descricao">
-                {/*
+            {product.table && (
+              <div onClick={() => toggleImage(product.table)}>
+                <img src={getImage(product.table)} />
+              </div>
+            )}
+          </div>
+
+          <div className="img-selecionada">
+            <img src={getImage(selectedImage)} />
+          </div>
+        </section>
+
+        <section className="produto-info">
+          <h2>{product.name}</h2>
+          {product.flavor && (
+            <div>
+              <h3>Sabor</h3>
+              <p>{product.flavor}</p>
+            </div>
+          )}
+          {product.weight && (
+            <div>
+              <h3>Peso</h3>
+              <p>{product.weight}</p>
+            </div>
+          )}
+        </section>
+        <div className="produto-compras">
+          <section className="produto-disponivel">
+            <h3>{product.available ? "Em estoque" : "Indisponível"}</h3>
+            {isLoggedIn && (
+              <input
+                type="checkbox"
+                checked={available}
+                onChange={handleAvailableChange}
+              />
+            )}
+          </section>
+          <section className="produto-preco">
+            <h3>Preço</h3>
+            <p>{toReais(product.price)}</p>
+            <button onClick={whatsappRedirect}>
+              <FontAwesomeIcon icon={faWhatsapp} />
+              COMPRE AGORA
+            </button>
+          </section>
+        </div>
+      </div>
+
+      <section className="produto-descricao">
+        {/*
                 <h2>Descrição</h2>
                 <p>{ product.description }</p>
                 */}
-            </section>
-            
-        </main>
-    )
-  }
-  
-  export default PaginaDetalhes
+      </section>
+    </main>
+  );
+};
+
+export default PaginaDetalhes;
