c46fd0183e812e3fbe488e227ac50074078c5641
Kledson Vinicius <kledsonvinicius0702@gmail.com>
2024-04-17 17:34:08 -0300

    fix bug 134.0

--- frontend/src/api/LoginApi.ts
+++ frontend/src/api/LoginApi.ts
@@ -1,15 +1,15 @@
-const api_url: string = "https://main--rococo-druid-6fe908.netlify.app"
+const api_url: string = "https://main--rococo-druid-6fe908.netlify.app";
 
 export const signIn = (email: string, password: string) => {
-    return fetch(`${api_url}/user/signin`, {
-            method: "POST",
-            body: JSON.stringify({ email: email, password: password }),
-            headers: { "Content-type": "application/json" },
-          })
-          .then((res) => res.json())
-          .then((data) => {
-            let token: string = ""
-            if(data.token) token = data.token
-            return token
-          })
-}
+  return fetch(`${api_url}/user/signin`, {
+    method: "POST",
+    body: JSON.stringify({ email: email, password: password }),
+    headers: { "Content-type": "application/json" },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      let token: string = "";
+      if (data.token) token = data.token;
+      return token;
+    });
+};
--- frontend/src/pages/PaginaProdutos.tsx
+++ frontend/src/pages/PaginaProdutos.tsx
@@ -1,117 +1,120 @@
-import './PaginaProdutos.css'
-import { Link, useLocation } from 'react-router-dom'
-import ProdutoCard from '../components/ProdutoCard'
-import { BaseSyntheticEvent, useEffect, useState } from 'react'
-import { TypeProduct } from '../utils/Types'
-import { getAllProducts, getAllProductsAdm, getProductsByCategory, getProductsByName } from '../api/ProductsApi'
+import "./PaginaProdutos.css";
+import { Link, useLocation } from "react-router-dom";
+import ProdutoCard from "../components/ProdutoCard";
+import { BaseSyntheticEvent, useEffect, useState } from "react";
+import { TypeProduct } from "../utils/Types";
+import {
+  getAllProducts,
+  getAllProductsAdm,
+  getProductsByCategory,
+  getProductsByName,
+} from "../api/ProductsApi";
 
 type PropTypes = {
-    isLoggedIn: boolean
-}
+  isLoggedIn: boolean;
+};
 
-const PaginaProdutos = ({ isLoggedIn }:PropTypes) => {
-    const select: HTMLSelectElement | null = document.querySelector("#select-ordenacao")
+const PaginaProdutos = ({ isLoggedIn }: PropTypes) => {
+  const select: HTMLSelectElement | null =
+    document.querySelector("#select-ordenacao");
 
-    const location = useLocation();
-    const query_params = new URLSearchParams(location.search);
-    const categoria = query_params.get("categoria")
-    const pesquisa = query_params.get("pesquisa")
+  const location = useLocation();
+  const query_params = new URLSearchParams(location.search);
+  const categoria = query_params.get("categoria");
+  const pesquisa = query_params.get("pesquisa");
 
-    useEffect(() => {
-        if(select) select.value = "asc"
+  useEffect(() => {
+    if (select) select.value = "asc";
 
-        loadProducts()
-    }, [categoria, pesquisa, isLoggedIn]);
+    loadProducts();
+  }, [categoria, pesquisa, isLoggedIn]);
 
-    const [titulo, setTitulo] = useState<string>("")
-    const [products, setProducts] = useState<TypeProduct[]>([])
+  const [titulo, setTitulo] = useState<string>("");
+  const [products, setProducts] = useState<TypeProduct[]>([]);
 
-    const handleOrdenacaoChange = (e: BaseSyntheticEvent) => {
-        const ordenacao: string = e.target.value
-        
-        if(categoria) {
-            getProductsByCategory(ordenacao, categoria)
-                .then(data => setProducts(data))
-        } else {
-            getAllProducts(ordenacao)
-                .then(data => setProducts(data))
-        }
+  const handleOrdenacaoChange = (e: BaseSyntheticEvent) => {
+    const ordenacao: string = e.target.value;
+
+    if (categoria) {
+      getProductsByCategory(ordenacao, categoria).then((data) =>
+        setProducts(data)
+      );
+    } else {
+      getAllProducts(ordenacao).then((data) => setProducts(data));
+    }
+  };
+
+  const renderOrdernacao = () => {
+    if (isLoggedIn) {
+      return false;
+    } else {
+      if (pesquisa) return false;
+    }
+    return true;
+  };
+
+  const loadProducts = () => {
+    const token = localStorage.getItem("token");
+
+    if (isLoggedIn && token) {
+      setTitulo("TODOS OS PRODUTOS");
+      getAllProductsAdm(token).then((data) => setProducts(data));
+      return;
+    }
+    if (categoria) {
+      setTitulo(categoria);
+      getProductsByCategory("asc", categoria).then((data) => setProducts(data));
+      return;
+    }
+    if (pesquisa) {
+      setTitulo(`Resultado da pesquisa '${pesquisa}'`);
+      getProductsByName(pesquisa).then((data) => setProducts(data));
+      return;
     }
 
-    const renderOrdernacao = () => {
-        if(isLoggedIn) {
-            return false
-        } else {
-            if(pesquisa) return false
-        }
-        return true
-    }
+    setTitulo("TODOS OS PRODUTOS");
+    getAllProducts("asc").then((data) => setProducts(data));
+    return;
+  };
 
-    const loadProducts = () => {
-        const token = localStorage.getItem("token")
+  return (
+    <main className="pagina-produtos">
+      <nav className="produtos-breadcrumbs">
+        <Link to={"/"}>Página Inicial</Link>
+        {">"}
+        <Link to={"/produtos"}>Produtos</Link>
+        {categoria && ">"}
+        {categoria && <Link to={`/produtos/${categoria}`}>{categoria}</Link>}
+      </nav>
 
-        if(isLoggedIn && token) {
-            setTitulo("TODOS OS PRODUTOS")
-            getAllProductsAdm(token)
-                .then(data => setProducts(data))
-            return
-        }
-        if(categoria) {
-            setTitulo(categoria)
-            getProductsByCategory("asc", categoria)
-                .then(data => setProducts(data))
-            return
-        }
-        if(pesquisa) {
-            setTitulo(`Resultado da pesquisa '${pesquisa}'`)
-            getProductsByName(pesquisa)
-                .then(data => setProducts(data))
-            return
-        }
+      <div className="produtos-container">
+        <h2>{titulo}</h2>
 
-        setTitulo("TODOS OS PRODUTOS")
-        getAllProducts("asc")
-                .then(data => setProducts(data))
-        return 
-    }
+        <section className="produtos">
+          <div className="ordenacao-container">
+            {renderOrdernacao() && (
+              <div>
+                <label htmlFor="order">Ordenação: </label>
+                <select
+                  id="select-ordenacao"
+                  name="order"
+                  onChange={handleOrdenacaoChange}
+                >
+                  <option value="asc">A - Z</option>
+                  <option value="desc">Z - A</option>
+                  <option value="price/asc">Menor preço</option>
+                  <option value="price/desc">Maior preço</option>
+                </select>
+              </div>
+            )}
+          </div>
+          {products.map((product) => {
+            return <ProdutoCard key={product._id} product={product} />;
+          })}
+        </section>
+      </div>
+    </main>
+  );
+};
 
-    return (
-        <main className="pagina-produtos">
-            <nav className="produtos-breadcrumbs">
-                <Link to={"/"}>Página Inicial</Link>
-                { ">" }
-                <Link to={"/produtos"}>Produtos</Link>
-                { categoria && ">" }
-                { categoria && <Link to={`/produtos/${categoria}`}>{ categoria }</Link> }
-            </nav>
-
-            <div className="produtos-container">
-                <h2>
-                    { titulo }
-                </h2>
-                
-                <section className="produtos">
-                    <div className="ordenacao-container">
-                        { renderOrdernacao() &&
-                        <div>
-                            <label htmlFor="order">Ordenação: </label>
-                            <select id="select-ordenacao" name="order" onChange={ handleOrdenacaoChange }>
-                                <option value="asc">A - Z</option>
-                                <option value="desc">Z - A</option>
-                                <option value="price/asc">Menor preço</option>
-                                <option value="price/desc">Maior preço</option>
-                            </select>
-                        </div>
-                        }
-                    </div>
-                    { products.map((product) => {
-                        return <ProdutoCard key={ product._id } product={ product }/>
-                    }) }
-                    
-                </section>
-            </div>
-        </main>
-    )
-  }
-  
-  export default PaginaProdutos
+export default PaginaProdutos;
