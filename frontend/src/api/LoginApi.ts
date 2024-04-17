6f0ab8de743e5d39d3765a4173492c04c9796c82
Kledson Vinicius <kledsonvinicius0702@gmail.com>
2024-04-17 17:14:35 -0300

    fix api_url

--- frontend/src/api/LoginApi.ts
+++ frontend/src/api/LoginApi.ts
@@ -1,4 +1,4 @@
-const api_url: string = "http://localhost:3001"
+const api_url: string = "https://main--rococo-druid-6fe908.netlify.app"
 
 export const signIn = (email: string, password: string) => {
     return fetch(`${api_url}/user/signin`, {
