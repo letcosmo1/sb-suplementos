91de66567e78386af6c70fec3eff7b0e2c3705ac
Leticia Cosmo <letcosmogeller@gmail.com>
2024-05-13 18:08:52 -0300

    fixed preco placeholder, enhanced conditional render of comprar

--- frontend/src/components/ProdutoCard.tsx
+++ frontend/src/components/ProdutoCard.tsx
@@ -3,24 +3,21 @@ import { Link } from "react-router-dom";
 import { TypeProduct } from "../utils/Types";
 import { getImage } from "../utils/ImageUrl";
 import { toReais } from "../utils/StringFormat";
-import { useState } from "react";
+import { BaseSyntheticEvent, useState } from "react";
 
 type PropTypes = {
   product: TypeProduct;
 }
 
 const ProdutoCard = ({ product }: PropTypes) => {
-  const [comprarStyle, setComprarStyle] = useState({display: "none"})
-  const [valorStyle, setValorStyle] = useState({display: "block"})
+  const [showComprar, setShowComprar] = useState<boolean>(false)
 
-  const showComprarButton = () => {
-    setComprarStyle({display: "flex"})
-    setValorStyle({display: "none"})
-  }
-
-  const hideComprarButton = () => {
-    setComprarStyle({display: "none"})
-    setValorStyle({display: "block"})
+  const switchShowComprar = (e: BaseSyntheticEvent) => {
+    if(e.type === "mouseenter") {
+      setShowComprar(true)
+    } else {
+      setShowComprar(false)
+    }
   }
 
   const nomePlaceholderStyle = () => {
@@ -33,8 +30,18 @@ const ProdutoCard = ({ product }: PropTypes) => {
     return {}
   }
 
+  const precoPlaceholderStyle = () => {
+    if(product.price === 0) {
+      return {
+        height: 38,
+        backgroundColor: "var(--light-gray)"
+      }
+    }
+    return {}
+  }
+
   return (
-    <article className="produto-card" onMouseEnter={ showComprarButton } onMouseLeave={ hideComprarButton }>
+    <article className="produto-card" onMouseEnter={ switchShowComprar } onMouseLeave={ switchShowComprar }>
       <Link to={"/produto/" + product._id}>
         <div className="produto-img-container">
           <img src={getImage(product.image)} />
@@ -42,10 +49,19 @@ const ProdutoCard = ({ product }: PropTypes) => {
         <h3 style={ nomePlaceholderStyle() }>
           {product.name}
         </h3>
-        <p className="produto-valor" style={ valorStyle }>{toReais(product.price)}</p>
-        <div className="produto-comprar" style={ comprarStyle }>
-          <p>COMPRAR</p>
-        </div>
+        {
+          !showComprar &&
+          <p className="produto-valor" style={ precoPlaceholderStyle() }>
+            { product.price === 0 ? null : toReais(product.price) }
+          </p>
+        }
+        {
+          showComprar &&
+          <div className="produto-comprar">
+            <p>COMPRAR</p>
+          </div>
+        }
+        
       </Link>
     </article>
   );
